756fe83117692a19b04cbcba024031d3f840ed68
Ajay Sieunarine <38733594+jefroy@users.noreply.github.com>
2021-04-11 18:37:54 -0400

    add loadArray

--- main.cpp
+++ main.cpp
@@ -17,15 +17,13 @@ int numRounds = 0;
 
 // function protos
 string getDataString(int dataNum);
-bool loadArray(int arr[], int size, int dataNum);
+bool loadArray(int dataNum);
 void initArray(int arr[], int size);
 void printArray(int arr[], int size);
 
 // main
 int main() {
-    initArray(CASES, MAX_CASES);
-    initArray(ROUNDS, MAX_ROUNDS);
-
+    loadArray(16);
     printArray(CASES, MAX_CASES);
     cout << endl;
     printArray(ROUNDS, MAX_ROUNDS);
@@ -37,15 +35,34 @@ string getDataString(int dataNum){
     return DATA_DIR + BASE_DATA_FILE_NAME + to_string(dataNum) + DATA_FILE_EXT;
 }
 
-bool loadArray(int arr[], int size, int dataNum){
+bool loadArray(int dataNum){
+    numCases = dataNum;
     ifstream df;
     df.open(getDataString(dataNum));
     if (!df.is_open()){
         cout << "file could not be opened" << endl;
         return false;
     }
+    initArray(CASES, MAX_CASES);
+    int line;
+    df >> line;
+    int i = 1;
+    // load cases
+    while(i <= numCases){
+        CASES[i++] = line;
+        df >> line;
+    }
 
-    // load
+    // load rounds
+    initArray(ROUNDS, MAX_ROUNDS);
+    numRounds = line;
+    df >> line;
+    i = 1;
+    while(i <= numRounds){
+        int val = line;
+        ROUNDS[i++] = val;
+        df >> line;
+    }
     df.close();
 }
 
@@ -57,6 +74,9 @@ void initArray(int arr[], int size){
 
 void printArray(int arr[], int size){
     for (int i = 0; i < size; ++i) {
-        cout << arr[i];
+        if (i % 5 == 0){
+            cout << "\n";
+        }
+        cout << "\t" << arr[i];
     }
 }
